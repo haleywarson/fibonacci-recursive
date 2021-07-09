1b18581bf204dba72fce4e34fffa89adb080f161
Haley Warson <haleywarson@gmail.com>
2021-07-08 19:25:31 -0600

    Complete

--- javascript/fibonacci_recursive.js
+++ javascript/fibonacci_recursive.js
@@ -1,5 +1,13 @@
 function fibonacci(n) {
-  // type your code here
+  if (n === 0 || n === 1) {
+    return n;
+  }
+
+  if (n === 2) {
+    return 1;
+  }
+
+  return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
 if (require.main === module) {
